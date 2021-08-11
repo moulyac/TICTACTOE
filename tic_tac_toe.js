91b885730fe03f070f330b95c1932e2be9dbc15d
moulyac <87307264+moulyac@users.noreply.github.com>
2021-08-11 14:13:15 +0530

    second commit

---
+++ tic_tac_toe.js
@@ -0,0 +1,135 @@
+const boxes = document.querySelectorAll('.box')
+const result= document.querySelector('#result')
+const [button]= document.getElementsByTagName('button')
+ //console.log(boxes)
+const playerX = 'X'
+const playerO = 'O'
+let currentPlayer = playerX
+let XO = []
+let winstatus = false
+let drawstatus = false
+        
+const draw = ()=>{
+    let boxfilled = 0
+    //console.log('hhh')
+    XO.forEach((box,i)=>{
+        // console.log(space,i)
+        if(box !== null) boxfilled++
+    })
+    if(boxfilled === 9){
+        result.innerText = 'Its a DRAW!!'
+        button.innerText='new game'
+        drawstatus = true
+        return true
+    }
+}
+
+const win =(e)=>{
+    // const id = e.target.id
+    if(XO[0] === currentPlayer){
+        //console.log(XO[0],XO[1],XO[2])
+        if(XO[2] === currentPlayer && XO[1] === currentPlayer){
+            //console.log('hhhh')
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+        if(XO[3] === currentPlayer && XO[6] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+        if(XO[4] === currentPlayer && XO[8] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+    }
+
+    if(XO[4] === currentPlayer){
+        if(XO[1] === currentPlayer && XO[7] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+        if(XO[3] === currentPlayer && XO[5] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+        if(XO[6] === currentPlayer && XO[2] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+    }
+
+    if(XO[8] === currentPlayer){
+        if(XO[5] === currentPlayer && XO[2] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+        if(XO[6] === currentPlayer && XO[7] === currentPlayer){
+            result.innerText = `Player ${currentPlayer} won!!`
+            button.innerText='new game'
+            return true
+        }
+    }
+    return false
+}
+
+const restart=()=>{
+    XO=[]
+    boxes.forEach((box)=>{
+        box.innerText=''
+    })
+    result.innerText=''
+    button.innerText='restart'
+}
+
+const boxclick = (e)=>{
+    const id = e.target.id
+    if(!winstatus && !drawstatus){
+        if(!XO[id]){
+            //console.log(space[id])
+            XO[id] = currentPlayer
+            e.target.innerText = currentPlayer
+            if(win()){
+                winstatus= true
+                return 
+            }
+            if(draw()){
+                //console.log('draw')
+                return
+            }
+            currentPlayer = currentPlayer === playerO ? playerX : playerO
+            }
+        }
+        if(winstatus || drawstatus){
+            restart()
+            winstatus=false
+            drawstatus=false
+        }
+    }
+
+    const boxstyle = ()=>{
+        boxes.forEach((box,i)=>{
+            let styleboard = ''
+            if (i < 3) {
+                styleboard += 'border-bottom: 3px solid black;'
+            }
+            if (i % 3 === 0) {
+                styleboard += 'border-right: 3px solid black;'
+            }
+            if (i % 3 === 2) {
+                styleboard += 'border-left: 3px solid black;'
+            }
+            if (i > 5) {
+                styleboard += 'border-top: 3px solid black;'
+            }
+                box.style = styleboard
+                box.addEventListener('click',boxclick)
+            })
+        }
+boxstyle()
